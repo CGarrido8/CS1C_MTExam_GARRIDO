4e62a8751775cc87027d4be4a7db5f1c5398a812
Carlos Emmanuel Mercado Garrido <dabsyyy@icloud.com>
2025-03-21 18:29:25 +0800

    Add files via upload

---
+++ userProfile.js
@@ -0,0 +1,12 @@
+// Gather user information using prompt()
+let fullName = prompt("Enter your full name (first and last):");
+let age = prompt("Enter your age:");
+let favoriteNumber = prompt("Enter your favorite number:");
+let favoriteColor = prompt("Enter your favorite color:");
+
+// Display gathered information to the console
+console.log("User Information:");
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
