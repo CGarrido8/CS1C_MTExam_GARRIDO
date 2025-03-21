991be2bd8584eb905de42db8829471ab2327bc0f
Carlos Emmanuel Mercado Garrido <dabsyyy@icloud.com>
2025-03-21 18:36:46 +0800

    Add files via upload

---
+++ classSubjects.js
@@ -0,0 +1,9 @@
+// Gather user information using prompt()
+let studentName = prompt("Enter your name:");
+let subjectTitle = prompt("Enter the subject title:");
+let classSchedule = prompt("Enter the class schedule (Time, Days):");
+let classroom = prompt("Enter the classroom:");
+let classInstructor = prompt("Enter the class instructor's name:");
+
+// Display gathered information in the required format
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
